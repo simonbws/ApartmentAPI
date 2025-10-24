0c4d03eec87a8dc8438497ef9d08914bed8cca17
simonbws <simonbws6@gmail.com>
2025-10-25 00:22:36 +0200

    feat: AutoMapper implemented in API Controller

--- Apartment_API/Controllers/ApartmentAPIController.cs
+++ Apartment_API/Controllers/ApartmentAPIController.cs
@@ -1,6 +1,8 @@
 ﻿using Apartment_API.Data;
 using Apartment_API.Models;
 using Apartment_API.Models.DTO;
+using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,16 +14,19 @@ namespace Apartment_API.Controllers
     public class ApartmentAPIController : ControllerBase
     {
         private readonly AppDbContext _db;
-        public ApartmentAPIController(AppDbContext db)
+        private readonly IMapper _mapper;
+        public ApartmentAPIController(AppDbContext db, IMapper mapper)
         {
             _db = db;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<ApartmentDTO>> GetApartments()
         {
-            return Ok(await _db.Apartments.ToListAsync());
+            IEnumerable<Apartment> apartmentList = await _db.Apartments.ToListAsync();
+            return Ok(_mapper.Map<List<ApartmentDTO>>(apartmentList));
         }
 
         [HttpGet("{id:int}", Name = "GetApartment")]
@@ -39,44 +44,49 @@ namespace Apartment_API.Controllers
             {
                 return NotFound();
             }
-            return Ok(apartment);
+            return Ok(_mapper.Map<ApartmentDTO>(apartment));
         }
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<ApartmentDTO>> CreateApartment([FromBody] ApartmentCreateDTO apartmentDTO)
+        public async Task<ActionResult<ApartmentDTO>> CreateApartment([FromBody] ApartmentCreateDTO createDTO)
         {
             //if(!ModelState.IsValid)
             //{
             //    return BadRequest(ModelState); 
             //}
-            if (await _db.Apartments.FirstOrDefaultAsync(u => u.Name.ToLower() == apartmentDTO.Name.ToLower()) != null)
+            if (await _db.Apartments.FirstOrDefaultAsync(u => u.Name.ToLower() == createDTO.Name.ToLower()) != null)
             {
                 ModelState.AddModelError("CustomError", "Apartment already Exists!");
                 return BadRequest(ModelState);
             }
-            if (apartmentDTO == null)
+            if (createDTO == null)
             {
-                return BadRequest(apartmentDTO);
+                return BadRequest(createDTO);
             }
             //if (apartmentDTO.Id > 0)
             //{
             //    return StatusCode(StatusCodes.Status500InternalServerError);
             //}
+            
+
+            //Ale tutaj zamiast ręcznie wpisywać zakomentowane wartości poniżej, korzystamy z AutoMappera (funkcjonalność dodana póżniej)
+            Apartment model = _mapper.Map<Apartment>(createDTO);
+
             // mapujemy dane poniewaz musimy dodac DTO do bazy danych. DTO nie jest encja, więc EF Core nie wie jak to zapisać w SQL. Apartament to warstwa bazy danych DbSet<Apartment>
             // ApartmentDTO to warstwa API. Musimy zmapowaćDTO na model bazy danych (Apartment).
             //Używamy DTO, bo encje (model bazy) nie zawsze powinien wychodzić na zewnątrz API. DTO to filtr bezpieczeństwa i wygody między API, a bazą danych.
-            Apartment model = new()
-            {
-                Name = apartmentDTO.Name,
-                Details = apartmentDTO.Details,
-                ImageUrl = apartmentDTO.ImageUrl,
-                Occupancy = apartmentDTO.Occupancy,
-                Rate = apartmentDTO.Rate,
-                Sqft = apartmentDTO.Sqft,
-                Amenity = apartmentDTO.Amenity
-            };
+            //Apartment model = new()
+            //{
+            //    Name = createDTO.Name,
+            //    Details = createDTO.Details,
+            //    ImageUrl = createDTO.ImageUrl,
+            //    Occupancy = createDTO.Occupancy,
+            //    Rate = createDTO.Rate,
+            //    Sqft = createDTO.Sqft,
+            //    Amenity = createDTO.Amenity
+            //};
             await _db.Apartments.AddAsync(model);
             await _db.SaveChangesAsync();
 
@@ -105,9 +115,9 @@ namespace Apartment_API.Controllers
         [HttpPut("{id:int}", Name = "UpdateVilla")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> UpdateApartment(int id, [FromBody] ApartmentUpdateDTO apartmentDTO)
+        public async Task<IActionResult> UpdateApartment(int id, [FromBody] ApartmentUpdateDTO updateDTO)
         {
-            if (apartmentDTO == null || id != apartmentDTO.Id)
+            if (updateDTO == null || id != updateDTO.Id)
             {
                 return BadRequest();
             }
@@ -115,18 +125,19 @@ namespace Apartment_API.Controllers
             //villa.Name = villaDTO.Name;
             //villa.Sqft = villaDTO.Sqft;
             //villa.Occupancy = villaDTO.Occupancy;
-
-            Apartment model = new()
-            {
-                Name = apartmentDTO.Name,
-                Details = apartmentDTO.Details,
-                ImageUrl = apartmentDTO.ImageUrl,
-                Occupancy = apartmentDTO.Occupancy,
-                Rate = apartmentDTO.Rate,
-                Sqft = apartmentDTO.Sqft,
-                Amenity = apartmentDTO.Amenity,
-                CreatedDate = DateTime.Now
-            };
+            Apartment model = _mapper.Map<Apartment>(updateDTO);
+            //Auto Mapper
+            //Apartment model = new()
+            //{
+            //    Name = updateDTO.Name,
+            //    Details = updateDTO.Details,
+            //    ImageUrl = updateDTO.ImageUrl,
+            //    Occupancy = updateDTO.Occupancy,
+            //    Rate = updateDTO.Rate,
+            //    Sqft = updateDTO.Sqft,
+            //    Amenity = updateDTO.Amenity,
+            //    CreatedDate = DateTime.Now
+            //};
             _db.Apartments.Update(model);
             await _db.SaveChangesAsync();
             return NoContent();
@@ -142,33 +153,36 @@ namespace Apartment_API.Controllers
             }
             var apartment = await _db.Apartments.AsNoTracking().FirstOrDefaultAsync(u => u.Id == id);
 
-            ApartmentUpdateDTO apartmentDTO = new()
-            {
-                Amenity = apartment.Amenity,
-                Details = apartment.Details,
-                Id = apartment.Id,
-                ImageUrl = apartment.ImageUrl,
-                Name = apartment.Name,
-                Occupancy = apartment.Occupancy,
-                Rate = apartment.Rate,
-                Sqft = apartment.Sqft
-            };
+            ApartmentUpdateDTO apartmentDTO = _mapper.Map<ApartmentUpdateDTO>(apartment);
+            //ApartmentUpdateDTO apartmentDTO = new()
+            //{
+            //    Amenity = apartment.Amenity,
+            //    Details = apartment.Details,
+            //    Id = apartment.Id,
+            //    ImageUrl = apartment.ImageUrl,
+            //    Name = apartment.Name,
+            //    Occupancy = apartment.Occupancy,
+            //    Rate = apartment.Rate,
+            //    Sqft = apartment.Sqft
+            //};
             if (apartment == null)
             {
                 return BadRequest();
             }
             patchDTO.ApplyTo(apartmentDTO, ModelState);
-            Apartment model = new Apartment()
-            {
-                Amenity = apartmentDTO.Amenity,
-                Details = apartmentDTO.Details,
-                Id = apartmentDTO.Id,
-                ImageUrl = apartmentDTO.ImageUrl,
-                Name = apartmentDTO.Name,
-                Occupancy = apartmentDTO.Occupancy,
-                Rate = apartmentDTO.Rate,
-                Sqft = apartmentDTO.Sqft
-            };
+            //Auto Mapper, destination is Apartment and the source is apartmentDTO
+            Apartment model = _mapper.Map<Apartment>(apartmentDTO);
+            //Apartment model = new Apartment()
+            //{
+            //    Amenity = apartmentDTO.Amenity,
+            //    Details = apartmentDTO.Details,
+            //    Id = apartmentDTO.Id,
+            //    ImageUrl = apartmentDTO.ImageUrl,
+            //    Name = apartmentDTO.Name,
+            //    Occupancy = apartmentDTO.Occupancy,
+            //    Rate = apartmentDTO.Rate,
+            //    Sqft = apartmentDTO.Sqft
+            //};
             _db.Apartments.Update(model);
             await _db.SaveChangesAsync();
 
