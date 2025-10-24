f944127e66ff26edce93c5625f2d9ec9af50a439
simonbws <simonbws6@gmail.com>
2025-10-24 20:58:10 +0200

    feat: seed apartment table with records

--- Apartment_API/Data/AppDbContext.cs
+++ Apartment_API/Data/AppDbContext.cs
@@ -10,5 +10,70 @@ namespace Apartment_API.Data
             
         }
         public DbSet<Apartment> Apartments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Apartment>().HasData(
+                new Apartment
+                {
+                    Id = 1,
+                    Name = "Apartment Royale",
+                    Details = "Spacious apartment with modern amenities and a breathtaking city view.",
+                    ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa3.jpg",
+                    Occupancy = 4,
+                    Rate = 250,
+                    Sqft = 550,
+                    Amenity = "",
+                    CreatedDate = DateTime.Now
+                },
+              new Apartment
+              {
+                  Id = 2,
+                  Name = "Apartment Serenity",
+                  Details = "Cozy apartment perfect for a relaxing getaway with family and friends.",
+                  ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa1.jpg",
+                  Occupancy = 4,
+                  Rate = 300,
+                  Sqft = 550,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now
+              },
+              new Apartment
+              {
+                  Id = 3,
+                  Name = "Apartment Luxe",
+                  Details = "Luxury apartment featuring a private pool and elegant interior design.",
+                  ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa4.jpg",
+                  Occupancy = 4,
+                  Rate = 400,
+                  Sqft = 750,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now
+              },
+              new Apartment
+              {
+                  Id = 4,
+                  Name = "Apartment Diamond",
+                  Details = "Modern apartment with high-end appliances and stunning balcony views.",
+                  ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa5.jpg",
+                  Occupancy = 4,
+                  Rate = 550,
+                  Sqft = 900,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now
+              },
+              new Apartment
+              {
+                  Id = 5,
+                  Name = "Apartment Elegance",
+                  Details = "Elegant apartment located near the beach, ideal for a peaceful vacation.",
+                  ImageUrl = "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa2.jpg",
+                  Occupancy = 4,
+                  Rate = 600,
+                  Sqft = 1100,
+                  Amenity = "",
+                  CreatedDate = DateTime.Now
+              });
+        }
     }
 }
---
+++ Apartment_API/Migrations/20251024185522_SeedApartmentTableWithData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Apartment_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedApartmentTableWithData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Apartments",
+                columns: new[] { "Id", "Amenity", "CreatedDate", "Details", "ImageUrl", "Name", "Occupancy", "Rate", "Sqft", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, "", new DateTime(2025, 10, 24, 20, 55, 22, 930, DateTimeKind.Local).AddTicks(7393), "Spacious apartment with modern amenities and a breathtaking city view.", "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa3.jpg", "Apartment Royale", 4, 250.0, 550, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, "", new DateTime(2025, 10, 24, 20, 55, 22, 930, DateTimeKind.Local).AddTicks(7453), "Cozy apartment perfect for a relaxing getaway with family and friends.", "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa1.jpg", "Apartment Serenity", 4, 300.0, 550, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, "", new DateTime(2025, 10, 24, 20, 55, 22, 930, DateTimeKind.Local).AddTicks(7456), "Luxury apartment featuring a private pool and elegant interior design.", "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa4.jpg", "Apartment Luxe", 4, 400.0, 750, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 4, "", new DateTime(2025, 10, 24, 20, 55, 22, 930, DateTimeKind.Local).AddTicks(7458), "Modern apartment with high-end appliances and stunning balcony views.", "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa5.jpg", "Apartment Diamond", 4, 550.0, 900, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 5, "", new DateTime(2025, 10, 24, 20, 55, 22, 930, DateTimeKind.Local).AddTicks(7461), "Elegant apartment located near the beach, ideal for a peaceful vacation.", "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa2.jpg", "Apartment Elegance", 4, 600.0, 1100, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Apartments",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
