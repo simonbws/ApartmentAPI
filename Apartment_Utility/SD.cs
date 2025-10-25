8e9125ee8e99cbb076ce8f392c8fdbbe65fa5c07
simonbws <simonbws6@gmail.com>
2025-10-25 14:22:23 +0200

    feat: Utility Project for SD

---
+++ Apartment_Utility/SD.cs
@@ -0,0 +1,14 @@
+﻿namespace Apartment_Utility
+{
+    public static class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+
+    }
+}
---
+++ Apartment_Web/Models/DTO/ApartmentNumberCreateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Apartment_Web.Models.DTO
+{
+    public class ApartmentNumberCreateDTO
+    {
+        [Required]
+        public int ApartmentNo { get; set; }
+        [Required]
+        public int ApartmentID { get; set; }
+        public string SpecialDetails { get; set; }
+    }
+}
