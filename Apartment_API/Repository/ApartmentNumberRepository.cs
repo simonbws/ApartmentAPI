dea1d7e2564ce353fdedfe5ac90fe7c09a59e66f
simonbws <simonbws6@gmail.com>
2025-10-25 12:01:30 +0200

    feat: number repository with implementation

---
+++ Apartment_API/Repository/ApartmentNumberRepository.cs
@@ -0,0 +1,23 @@
+﻿using Apartment_API.Data;
+using Apartment_API.Models;
+using Apartment_API.Repository.IRepository;
+
+namespace Apartment_API.Repository
+{
+    public class ApartmentNumberRepository : Repository<ApartmentNumber>, IApartmentNumberRepository
+    {
+        private readonly AppDbContext _db;
+        public ApartmentNumberRepository(AppDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public async Task<ApartmentNumber> UpdateAsync(ApartmentNumber entity)
+        {
+            entity.UpdatedDate = DateTime.Now;
+            _db.ApartmentNumbers.Update(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
---
+++ Apartment_API/Repository/IRepository/IApartmentNumberRepository.cs
@@ -0,0 +1,9 @@
+﻿using Apartment_API.Models;
+
+namespace Apartment_API.Repository.IRepository
+{
+    public interface IApartmentNumberRepository : IRepository<ApartmentNumber>
+    {
+        Task<ApartmentNumber> UpdateAsync(ApartmentNumber entity);
+    }
+}
