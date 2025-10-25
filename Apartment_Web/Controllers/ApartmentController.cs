e9a170e650b6e883ce3989beebe73cc56a9e2f42
simonbws <simonbws6@gmail.com>
2025-10-25 17:06:37 +0200

    feat: Apartment Controller in Web Project

---
+++ Apartment_Web/Controllers/ApartmentController.cs
@@ -0,0 +1,32 @@
+﻿using Apartment_Web.Models;
+using Apartment_Web.Models.DTO;
+using Apartment_Web.Services.IServices;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Apartment_Web.Controllers
+{
+    public class ApartmentController : Controller
+    {
+        private readonly IApartmentService _apartmentService;
+        private readonly IMapper _mapper;
+        public ApartmentController(IApartmentService apartmentService, IMapper mapper)
+        {
+            _apartmentService = apartmentService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> IndexApartment()
+        {
+            List<ApartmentDTO> list = new();
+            var response = await _apartmentService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ApartmentDTO>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+    }
+}
