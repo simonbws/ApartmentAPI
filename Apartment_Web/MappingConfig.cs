67d296652f1fe7ef98d90b7b4851751d678c16da
simonbws <simonbws6@gmail.com>
2025-10-25 14:37:39 +0200

    feat: automapper

---
+++ Apartment_Web/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿
+using Apartment_Web.Models.DTO;
+using AutoMapper;
+
+namespace Apartment_Web
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig() 
+        {
+            CreateMap<ApartmentDTO, ApartmentCreateDTO>().ReverseMap();
+            CreateMap<ApartmentDTO, ApartmentUpdateDTO>().ReverseMap();
+
+            CreateMap<ApartmentNumberDTO, ApartmentNumberCreateDTO>().ReverseMap();
+            CreateMap<ApartmentNumberDTO, ApartmentNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
