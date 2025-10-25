8a0d7981733d643752f908fb0a3077e9c4cf9083
simonbws <simonbws6@gmail.com>
2025-10-25 10:28:02 +0200

    chore: Repository Clean Up

--- Apartment_API/Repository/ApartmentRepository.cs
+++ Apartment_API/Repository/ApartmentRepository.cs
@@ -6,59 +6,19 @@ using System.Linq.Expressions;
 
 namespace Apartment_API.Repository
 {
-    public class ApartmentRepository : IApartmentRepository
+    public class ApartmentRepository : Repository<Apartment>, IApartmentRepository
     {
         private readonly AppDbContext _db;
-        public ApartmentRepository(AppDbContext db)
+        public ApartmentRepository(AppDbContext db) : base(db)
         {
             _db = db;
         }
-        public async Task CreateAsync(Apartment entity)
+        public async Task<Apartment> UpdateAsync(Apartment entity)
         {
-            await _db.Apartments.AddAsync(entity);
-        }
-
-        public async Task<Apartment> GetAsync(Expression<Func<Apartment, bool>> filter = null, bool tracked = true)
-        {
-            IQueryable<Apartment> query = _db.Apartments;
-            if(!tracked)
-            {
-                query = query.AsNoTracking();
-            }
-            if(filter != null)
-            {
-                query = query.Where(filter);
-            }
-            // because this is single Get, we use FirstOrDefault instead of ToList.
-            return await query.FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Apartment>> GetAllAsync(Expression<Func<Apartment, bool>> filter = null)
-        {
-            IQueryable<Apartment> query = _db.Apartments;
-            if(filter != null)
-            {
-                query = query.Where(filter);
-            }
-            // At this point the query will be executed. This is deferred execution. ToList() causes immediate execution.
-            return await query.ToListAsync();
-        }
-
-        public async Task RemoveAsync(Apartment entity)
-        {
-            _db.Apartments.Remove(entity);
-            await SaveAsync();
-        }
-
-        public async Task SaveAsync()
-        {
-            await _db.SaveChangesAsync();
-        }
-
-        public async Task UpdateAsync(Apartment entity)
-        {
+            entity.UpdatedDate = DateTime.Now;
             _db.Apartments.Update(entity);
-            await SaveAsync(); ;
+            await _db.SaveChangesAsync();
+            return entity;
         }
     }
 }
---
+++ Apartment_API/Repository/Repository.cs
@@ -0,0 +1,61 @@
+﻿using Apartment_API.Data;
+using Apartment_API.Models;
+using Apartment_API.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Apartment_API.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(AppDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public async Task CreateAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await SaveAsync();
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            // because this is single Get, we use FirstOrDefault instead of ToList.
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            // At this point the query will be executed. This is deferred execution. ToList() causes immediate execution.
+            return await query.ToListAsync();
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            dbSet.Remove(entity);
+            await SaveAsync();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
