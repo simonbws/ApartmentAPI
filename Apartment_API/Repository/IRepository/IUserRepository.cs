f437af33915fab9716f73c043b4ba5a5f0302741
simonbws <simonbws6@gmail.com>
2025-10-27 21:36:20 +0100

    feat: IUserRepository

---
+++ Apartment_API/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Apartment_API.Models.DTO;
+using Apartment_API.Models;
+
+namespace Apartment_API.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
---
+++ Apartment_API/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Apartment_API.Models.DTO;
+using Apartment_API.Models;
+using Apartment_API.Data;
+
+namespace Apartment_API.Repository
+{
+    public class UserRepository
+    {
+        private readonly AppDbContext _db;
+
+        public UserRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
