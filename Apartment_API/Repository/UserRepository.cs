71bc9889b2a77cd67693289992c8af9d4c82e6e1
simonbws <simonbws6@gmail.com>
2025-10-29 22:43:23 +0100

    feat: registeration additional config

--- Apartment_API/Repository/UserRepository.cs
+++ Apartment_API/Repository/UserRepository.cs
@@ -15,16 +15,17 @@ namespace Apartment_API.Repository
     {
         private readonly AppDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private string secretKey;
         private readonly IMapper _mapper;
 
-        public UserRepository(AppDbContext db, IConfiguration configuration, UserManager<ApplicationUser> userManager, IMapper mapper)
+        public UserRepository(AppDbContext db, IConfiguration configuration, UserManager<ApplicationUser> userManager, IMapper mapper, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
             _mapper = mapper;
             _userManager = userManager;
             secretKey = configuration.GetValue<string>("ApiSettings:Secret");
-
+            _roleManager = roleManager;
         }
 
         public bool IsUniqueUser(string username)
@@ -92,18 +93,28 @@ namespace Apartment_API.Repository
                 var result = await _userManager.CreateAsync(user, registerationRequestDTO.Password);
                 if (result.Succeeded)
                 {
+                    if (!_roleManager.RoleExistsAsync("admin").GetAwaiter().GetResult())
+                    {
+                        await _roleManager.CreateAsync(new IdentityRole("admin"));
+                        await _roleManager.CreateAsync(new IdentityRole("customer"));
+                    }
                     await _userManager.AddToRoleAsync(user, "admin");
                     var userToReturn = _db.ApplicationUsers
                         .FirstOrDefault(u => u.UserName == registerationRequestDTO.UserName);
                     return _mapper.Map<UserDTO>(userToReturn);
 
                 }
+                else
+                {
+                    var errors = string.Join(", ", result.Errors.Select(e => e.Description));
+                    return new UserDTO { Name = $"Error: {errors}" };
+                }
             }
             catch (Exception e)
             {
-
+                return new UserDTO { Name = $"Exception: {e.Message}" };
             }
-            return new UserDTO();
+            
             
         }
     }
